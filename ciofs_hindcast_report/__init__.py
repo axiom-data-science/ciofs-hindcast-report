2c06a822889d48ef6b5d526f4fb6cc526a97b6cd
Kristen Thyng <kthyng@gmail.com>
2023-03-27 09:16:25 -0500

    forgot init file

---
+++ ciofs_hindcast_report/__init__.py
@@ -0,0 +1,8 @@
+
+import pathlib
+base = pathlib.Path(__path__[0])
+
+PATH_INPUTS = base / "inputs"
+PATH_INPUTS_RIVER = base / "inputs" / "river"
+PATH_OUTPUTS = base / "outputs"
+PATH_OUTPUTS_RIVER = PATH_OUTPUTS / "river"
